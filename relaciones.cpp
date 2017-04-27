ded94dcd4cde4ecee6c8427cc079cad00ab4aea5
Ariel Trippel <trippea77@gmail.com>
2017-04-27 20:17:35 -0300

    Update relaciones.cpp
    Agregada transitividad

--- relaciones.cpp
+++ relaciones.cpp
@@ -71,6 +71,110 @@ int buscarChar(char val,char c[]){
         }
 	}
 }
+
+void calcularDominio(int m[4][4],int c[]){
+    int i,j,a,cont=0;
+    int dom[4];
+    for(i=0;i<4;i++){
+        for(j=0;j<4;j++){
+            if(m[i][j]==1){
+                dom[cont]=c[i];
+                cont++;
+            }
+        }
+    }
+    printf("\nEl dominio es ");
+    for(int a = 0; a < cont; a++) printf(" %d", dom[a]);
+}
+void calcularImagen(int m[4][4],char c[]){
+    int i,j,a,cont=0,l,encontro;
+    char dom[4];
+    for(i=0;i<4;i++){
+        for(j=0;j<4;j++){
+            if(m[i][j]==1){
+                encontro=0;
+                for(l=0;l<=cont;l++){
+                    if(dom[l]==c[j]){
+                        encontro=1;
+                    }
+                }
+                if(encontro==0){
+                    dom[cont]=c[j];
+                        cont++;
+                }
+            }
+        }
+    }
+    printf("\nLa imagen es ");
+    for(int a = 0; a < cont; a++) printf(" %c", dom[a]);
+}
+int determinarRango(int m[4][4]){
+    int i,j,a,cont=0;
+    char dom[4];
+    for(i=0;i<4;i++){
+        for(j=0;j<4;j++){
+            if(m[i][j]==1){
+                cont++;
+            }
+        }
+    }
+    printf("\nEl rango es %d\n",cont);
+}
+void esReflexiva(int m[4][4]){
+    int i,cont=0;
+    for(i=0;i<4;i++){
+            if(m[i][i]==1){
+                cont++;
+            }
+    }
+    if(cont==4){
+        printf("Es reflexiva!!\n");
+        }else{
+        printf("No es reflexiva!!\n");
+        }
+    
+}
+int esAntisimetrica(int m[4][4]){
+    int i,j,cont=1;
+    for(i=0;i<4;i++){
+        for(j=0;j<4;j++){
+            if(((m[i][j]+m[j][i])==2)&&(j!=i)){
+                cont=0;
+            }
+        }
+    }
+    if(cont!=0){
+        printf("Es antisimetrica!!\n");
+        }else{
+        printf("No es antisimetrica!!\n");
+        }    
+}
+int esTransitiva(int m[4][4]){
+    int i,j,l,transitiva=1;
+    
+    for(i=0;i<4;i++){
+        for(j=0;j<4;j++){
+            if(m[i][j]==1){
+                for(l=0;l<4;l++){
+                    if(m[j][l]==1){
+                        if(m[i][l]!=1){
+                            transitiva=0;
+                        }
+                    }else{
+                        transitiva=1;
+                    }     
+            }
+            transitiva=0;
+        }
+    }
+    }
+    if(transitiva==1){
+        printf("Es transitiva!!\n");
+        }else{
+        printf("No es transitiva!!\n");
+        }  
+}
+
 int main(){
 	int conjunto1[4]={1,2,3,8};
 	char conjunto2[4]={'a','b','j','k'};
@@ -106,7 +210,12 @@ int main(){
         }
         printf("\n");
     }
-	
+    calcularDominio(relacion,conjunto1);
+	calcularImagen(relacion,conjunto2);
+	determinarRango(relacion);
+	esReflexiva(relacion);
+	esAntisimetrica(relacion);
+	esTransitiva(relacion);
 	system("pause");
 	return 0;
 }
