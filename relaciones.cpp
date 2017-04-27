929fb39edd0fc2d816faae785e8606aa65b2cf50
Ariel Trippel <trippea77@gmail.com>
2017-04-27 19:06:05 -0300

    Update relaciones.cpp
    Corregido muestra de relacion

--- relaciones.cpp
+++ relaciones.cpp
@@ -20,15 +20,13 @@ void mostrarConjuntos(int c1[],char c2[]){
 }
 int verificarInt(int val,int c[]){
     int j=1;
-       
    	for(int i=0;i<sizeof(c);i++){
 		if(c[i]==val){
             j=0;
-            break;
         }
 	}
-	if(!j){
-	printf("Ingrese un valor perteneciente al conjunto.\n");
+	if(j!=0){
+	printf("\nIngrese un valor perteneciente al conjunto.\n");
 	}
 	return j;
 }
@@ -37,40 +35,77 @@ int verificarChar(char val,char c[]){
        for(int i=0;i<sizeof(c);i++){
 		if(c[i]==val){
             j= 0;
-            break;
         }
 	}
-	if(!j){
-	printf("Ingrese un valor perteneciente al conjunto.\n");
+	if(j!=0){
+	printf("\nIngrese un valor perteneciente al conjunto.\n");
 	}
 	return j;
 }
-void pedirElemento1(int *val1){
-        printf("Ingrese el valor del primer conjunto:\n");  
-        scanf("%d",*val1);
+int pedirElemento1(){
+    int val1;
+        printf("\nIngrese el valor del primer conjunto:\n");  
+        scanf("%d",&val1);
+        return val1;
 }
-void pedirElemento2(char *val2){        
-        printf("Ingrese el valor del segundo conjuto:\n");
+char pedirElemento2(){  
+    char val2;      
+        printf("\nIngrese el valor del segundo conjuto:\n");
         fflush(stdin);
-        scanf("%c",*val2);    
+        scanf("%c",&val2);    
+    return val2;
+}
+int buscarInt(int val,int c[]){
+   	for(int i=0;i<sizeof(c);i++){
+		if(c[i]==val){
+            return i;
+            break;
+        }
+	}
+}
+int buscarChar(char val,char c[]){
+       for(int i=0;i<sizeof(c);i++){
+		if(c[i]==val){
+            return i;
+            break;
+        }
+	}
 }
 int main(){
 	int conjunto1[4]={1,2,3,8};
 	char conjunto2[4]={'a','b','j','k'};
-	int relacion[4][4];
-	int val1;
+	int relacion[4][4]={{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+	int val1,x,y,i,j,cont,cant;
     char val2;
 	
+	
+	
 	mostrarConjuntos(conjunto1,conjunto2);
+	printf("\nCuantas relaciones desea ingresar?\n");
+	scanf("%d",&cant);
 	
-    do{
-        pedirElemento1(&val1);
-	}while(verificarInt(val1,conjunto1));
-	do{
-        pedirElemento2(&val2);
-    }while(verificarChar(val2,conjunto2));
+	if(cant>16){
+        cant=16;
+    }
 	
-
+	for(cont=0;cont<cant;cont++){
+        do{
+            val1=pedirElemento1();
+    	}while(verificarInt(val1,conjunto1)!=0);
+    	do{
+            val2=pedirElemento2();
+        }while(verificarChar(val2,conjunto2)!=0);
+    	
+    	x=buscarInt(val1,conjunto1);
+    	y=buscarChar(val2,conjunto2);
+        relacion[x][y]=1;
+    }
+	for(i=0;i<4;i++){
+        for(j=0;j<4;j++){
+            printf("%d ",relacion[i][j]);
+        }
+        printf("\n");
+    }
 	
 	system("pause");
 	return 0;
